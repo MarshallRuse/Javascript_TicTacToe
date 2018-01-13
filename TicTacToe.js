6fec17108dedd644e9e6470f895be7dfd254b25e
Marshall Ruse <12mr9@queensu.ca>
2018-01-13 11:59:25 -0500

    The TicTacToe game I've been working on for a couple days

---
+++ TicTacToe.js
@@ -0,0 +1,313 @@
+var players = [];
+
+var Tile = (pos, move) => {
+
+	let position = pos;
+	let moveMarker = move;
+	const makeMove = (newMark) => {
+		moveMarker = newMark;
+	}
+
+	const getPosition = () => {
+		return position;
+	}
+
+	const getMoveMarker = () => {
+		return moveMarker;
+	}
+	return {makeMove, getPosition, getMoveMarker};
+}
+
+var gameBoard = (() => {
+
+	var board = {};
+	board.positions = new Array(9);
+	
+	var squares = document.querySelectorAll(".tile");
+	for (let i = 0; i < squares.length; i++){
+		let tile = Tile(squares[i].getAttribute("data-position-num"), i);
+		board.positions[i] = tile;
+	}
+
+	const winningState = () => {
+		//horizontal wins
+		if (board.positions[0].getMoveMarker() === board.positions[1].getMoveMarker() && 
+			board.positions[1].getMoveMarker() === board.positions[2].getMoveMarker()){
+			console.log("top-horiz");
+			return true;
+		}
+		else if (board.positions[3].getMoveMarker() === board.positions[4].getMoveMarker() && 
+			board.positions[4].getMoveMarker() === board.positions[5].getMoveMarker()){
+			console.log("mid-horiz");
+
+			return true
+		}
+		else if (board.positions[6].getMoveMarker() === board.positions[7].getMoveMarker() && 
+			board.positions[7].getMoveMarker() === board.positions[8].getMoveMarker()){
+						console.log("bottom-horiz");
+
+			return true;
+		}
+		//vertical wins
+		else if (board.positions[0].getMoveMarker() === board.positions[3].getMoveMarker() && 
+			board.positions[3].getMoveMarker() === board.positions[6].getMoveMarker()){
+						console.log("left-vert");
+
+			return true;
+		}
+		else if (board.positions[1].getMoveMarker() === board.positions[4].getMoveMarker() && 
+			board.positions[4].getMoveMarker() === board.positions[7].getMoveMarker()){
+						console.log("mid-vert");
+
+			return true;
+		}
+		else if (board.positions[2].getMoveMarker() === board.positions[5].getMoveMarker() && 
+			board.positions[5].getMoveMarker() === board.positions[8].getMoveMarker()){
+						console.log("bottom-vert");
+
+			return true;
+		}
+		//diagonal wins
+		else if (board.positions[0].getMoveMarker() === board.positions[4].getMoveMarker() && 
+			board.positions[4].getMoveMarker() === board.positions[8].getMoveMarker()){
+						console.log("left-right diag");
+
+			return true;
+		}
+		else if (board.positions[2].getMoveMarker() === board.positions[4].getMoveMarker() && 
+			board.positions[4].getMoveMarker() === board.positions[6].getMoveMarker()){
+						console.log("right-left diag");
+
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
+
+	const isLegalMove = (position) => {
+		if (board.positions[position] !== ""){
+			return false;
+		}
+		else {
+			return true;
+		}
+
+	}
+
+	const makeMove = (position, moveMarker) => {
+		if (isLegalMove){
+			moveMarker = moveMarker.toUpperCase();
+			board.positions[position].makeMove(moveMarker);
+			squares[position].innerHTML = moveMarker;
+			GamePlay.changeTurn();
+			return true; //legal move made
+		}
+		return false; //legal move not made
+	}
+
+	return {board, makeMove, winningState};
+})();
+
+var Player = (num, name, symbol) => {
+
+	let playerNumber = num;
+	let playerName = name;
+	let marker = symbol;
+
+	const getName = () => {
+		return playerName;
+	}
+
+	const getNumber = () => {
+		return playerNumber;
+	}
+
+	const getMarker = () => {
+		return marker;
+	}
+
+	return {getName, getNumber, getMarker};
+}
+
+var PlayerForm = (() => {
+
+	var symbolChosen = false;
+
+	const decideOtherPlayerSymbol = (num) => {
+
+		let p1SymbolChoice = document.querySelector("#player1Form").querySelectorAll("input[type=radio]");
+		let p2SymbolChoice = document.querySelector("#player2Form").querySelectorAll("input[type=radio]");
+		if (num == 1 && !symbolChosen){
+			if (p1SymbolChoice[0].checked){ // p1 chose X
+				p2SymbolChoice[1].checked = true;
+				p2SymbolChoice.forEach(function(radio){
+					radio.disabled = true;
+				});
+				symbolChosen = true;
+			}
+			else{
+				p2SymbolChoice[0].checked = true;
+				p2SymbolChoice.forEach(function(radio){
+					radio.disabled = true;
+				});
+				symbolChosen = true;
+			}
+		}
+		else if (num == 2 && !symbolChosen){
+			if (p2SymbolChoice[0].checked){ // p1 chose X
+				p1SymbolChoice[1].checked = true;
+				p1SymbolChoice.forEach(function(radio){
+					radio.disabled = true;
+				});
+				symbolChosen = true;
+			}
+			else{
+				p1SymbolChoice[0].checked = true;
+				p1SymbolChoice.forEach(function(radio){
+					radio.disabled = true;
+				});
+				symbolChosen = true;
+			}
+		}
+
+	}
+
+
+	const revealPlayerCard = (form, name, symbol) => {
+		form.style.display = "none";
+		var cardId = form.getAttribute("data-player-card-id");
+		var card = document.querySelector(cardId);
+		var info = card.querySelectorAll("h2");
+		console.log(info[0]);
+		info[0].innerHTML += name;
+		info[1].innerHTML += symbol;
+		card.style.display = "block";
+	}
+
+	const getPlayerInfo = (form) => {
+		var num = form.getAttribute("data-player-num");
+		var name = form.querySelector(".playerName").value;
+		var symbol = form.querySelector("input[type=radio]:checked").value;
+
+		var player = Player(num, name, symbol);
+		decideOtherPlayerSymbol(num);
+		revealPlayerCard(form, name, symbol);
+		return player;
+	}
+
+	return {getPlayerInfo};
+
+})();
+
+
+
+function addListeners(){
+	//Player Form 
+	var playerForms = document.querySelectorAll(".playerForm");
+	for (let i = 0; i < playerForms.length; i++){
+		playerForms[i].querySelector(".readyButton").addEventListener("click", function(e){
+			e.preventDefault();
+			players[i] = PlayerForm.getPlayerInfo(playerForms[i]);
+		});
+	}
+
+	//Game board tiles
+	var gameTiles = document.querySelectorAll(".tile");
+	for (let i = 0; i < gameTiles.length; i++){
+		gameTiles[i].addEventListener("click", function(e){
+			var pos = this.getAttribute("data-position-num");
+			console.log("current symbol is: " + GamePlay.currentPlayerSymbol());
+			gameBoard.makeMove(pos, GamePlay.currentPlayerSymbol());
+		});
+	}
+
+	//Start button
+	document.querySelector("#startButton").addEventListener("click", function(e){
+		GamePlay.start();
+	});
+
+}
+
+var GamePlay = (() => {
+	var p1Score = 0;
+	var p2Score = 0;
+	var turnCount = 0; //max 9 turns to win or its a tie
+
+	var isP1Turn;
+
+	const flipCoin = () => {
+		let num = Math.random();
+		if (num < 0.5){
+			isP1Turn = true
+		}
+		else {
+			isP1Turn = false;
+		}
+	}
+
+	const currentPlayerSymbol = () => {
+		if (isP1Turn){
+			return players[0].getMarker();
+		}
+		else {
+			return players[1].getMarker();
+		}
+	}
+
+	const nextTurnSymbol = () => {
+		
+	}
+
+	var changeTurn = () => {
+		displayTurn();
+		console.log(gameBoard.winningState());
+		if (gameBoard.winningState() || turnCount >= 8){
+			stop();
+		}
+		else{
+			turnCount++;
+
+			if (isP1Turn){
+				isP1Turn = false;
+			}
+			else {
+				isP1Turn = true;
+			}
+		}
+	}
+
+	const displayTurn = () => {
+		var turnDisplay = document.querySelector("#turnDisplay").querySelector("h2");
+		turnDisplay.innerHTML = currentPlayerSymbol();
+
+	}
+
+	const start = () => {
+		flipCoin(); //random first turn
+
+		
+	}
+
+	const stop = () => {
+		if (gameBoard.winningState()){
+			var playerName;
+			if (isP1Turn){
+				playerName = players[0].getName();
+			}
+			else {
+				playerName = players[1].getName()
+			}
+			alert(playerName + " wins!");
+		}
+		else {
+			alert("The game was a tie, that sucks");
+		}
+	}
+
+	return {start, changeTurn, currentPlayerSymbol};
+
+})();
+
+addListeners();
